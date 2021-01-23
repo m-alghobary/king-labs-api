7816f8cd3ddf47897835ba3aeea71e9b182fcba7
m-alghobary <m.alghobary90@gmail.com>
2021-01-23 18:50:15 +0300

    add company model & migration

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'phone' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->companyEmail,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ];
+    }
+}
